76cc4d52c1602b0e0024fbda7ab4f39a46a44f12
Yui <50331474+SirYodaJedi@users.noreply.github.com>
2026-01-18 20:42:02 -0500

    gamepacks - don't search MP3 sounds for GoldSrc (#12)

--- cmake/gamepacks.cmake
+++ cmake/gamepacks.cmake
@@ -64,7 +64,7 @@ function(add_gamepack name)
   archivetypes="$<IF:$<TARGET_PROPERTY:GOLDSRC>,pak wad,vpk gma gcf>"
   texturetypes="$<IF:$<TARGET_PROPERTY:GOLDSRC>,hlw spr mdl,vtf tth>"
   modeltypes="mdl"
-  soundtypes="wav mp3"
+  soundtypes="$<IF:$<TARGET_PROPERTY:GOLDSRC>,wav,wav mp3>"
   maptypes="$<IF:$<TARGET_PROPERTY:GOLDSRC>,maphl,mapvmf>"
   shaders="$<IF:$<TARGET_PROPERTY:GOLDSRC>,quake3,source>"
   entityclass="quake3"
